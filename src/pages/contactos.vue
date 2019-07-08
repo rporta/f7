b8cf4a3ad8cdea3bedfd0f84d12c30c6815224f2
Your Name <you@example.com>
2019-07-08 01:32:02 -0300

    se agrega formulario de contactos

--- src/pages/contactos.vue
+++ src/pages/contactos.vue
@@ -5,10 +5,54 @@
         <f7-link icon-ios="f7:menu" icon-aurora="f7:menu" icon-md="material:menu" panel-open="right"></f7-link>
       </f7-nav-right>
     </f7-navbar>
-    <f7-block strong>
-      <p>Sorry</p>
-      <p>Requested content not found.</p>
-    </f7-block>
+      <f7-list no-hairlines-md>
+        <f7-list-input
+          outline
+          label="Nobre"
+          floating-label
+          type="text"
+          placeholder="Tu nombre"
+          clear-button
+        >
+          <f7-icon f7="edit" slot="media"></f7-icon>
+        </f7-list-input>
+        <f7-list-input
+          outline
+          label="Apellido"
+          floating-label
+          type="text"
+          placeholder="Tu Apellido"
+          clear-button
+        >
+          <f7-icon f7="edit" slot="media"></f7-icon>
+        </f7-list-input>
+        <f7-list-input
+          outline
+          label="E-mail"
+          floating-label
+          type="email"
+          placeholder="Tu correo"
+          clear-button
+        >
+          <f7-icon f7="edit" slot="media"></f7-icon>
+        </f7-list-input>
+        <f7-list-input
+          outline
+          label="Mensaje"
+          floating-label
+          type="textarea"
+          resizable
+          placeholder="Tu mensaje"
+          clear-button
+        >
+          <f7-icon f7="edit" slot="media"></f7-icon>
+        </f7-list-input>
+      </f7-list>
+      <!-- Toolbar-->
+      <f7-toolbar bottom>
+        <f7-link >Enviar</f7-link>
+        <f7-link >Cancelar</f7-link>
+      </f7-toolbar>
   </f7-page>
 </template>
 <script>
